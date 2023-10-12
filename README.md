b8bcfd1581ccb444aebd9d0a514fb222bbb2a0d6
lienlq <lien.lequang90@gmail.com>
2023-10-12 12:39:45 +0700

    start project into MainActivity add script build

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+# 12/10/2023 
+### Update build script
+* Script: Auto replace Production Ads ID
--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ rm app/build/outputs/bundle/appReleaseRelease/*
 ./gradlew assembleAppTestRelease
 cp app/build/outputs/apk/appTest/release/* output/
 
+./gradlew release
 ./gradlew bundleAppReleaseRelease
 cp app/build/outputs/bundle/appReleaseRelease/* output/
+./gradlew remove_ads_release
+
